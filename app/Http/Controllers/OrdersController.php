929a2dc12c8687e5287f660323409255308c9527
su <18334793246@163.com>
2018-07-24 15:08:28 +0800

    1

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -42,7 +42,7 @@ class OrdersController extends Controller
            
             $fields = $order->fields()->get();//订单买的 商品
             foreach ($fields as $ke => $field) {
-                 $date = $field->pivot->order_date;
+                $date = $field->pivot->order_date;
             dump($date);
             }
            
