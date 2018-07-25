e1f33d5ddf6da64d3cb6b85514d264dd544c6400
su <18334793246@163.com>
2018-07-25 17:09:46 +0800

    1

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -12,6 +12,7 @@ class LoginController extends Controller
 {
     public function index(Request $request)
     {
+
     		//主广告位
     	$advertisement_main = Advertisement::where('type',1)->orderBy('created_at','desc')->get();
     		//次广告位   2x2 栏
@@ -25,14 +26,24 @@ class LoginController extends Controller
     	
     	
     		//商家列表
-    	$stores = Store::where('switch',1)->take(5);
+    	$stores = Store::where('switch',1)->get()->take(5);
     	foreach ($stores as $key => $store) {
     			//	该商家的 运动品类
     		$type = $store->types()->get()->unique();
    
     			//	该商家的 平均 评分
     		$average = $store->estimates()->get()->pluck('average')->avg();
-    		dump($average);
+    		// dump($average);
+
+    			//免费体验标签
+    		$price_min = $store->fields()->pluck('price')->min();
+    		if(!$price_min || $price_min == 0){
+    			dump(88);
+    		}else{
+    			dump($price_min);
+    		}
+    		
+ 
 
     	}
     	
